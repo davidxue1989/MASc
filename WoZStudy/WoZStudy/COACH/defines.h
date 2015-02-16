94d2dba24df03d6e28288f5bfcb6e7e19f6016bd
davidxue1989 <davidxue1989@gmail.com>
2015-02-16 09:12:35 -0500

    minor
    minor

--- WoZStudy/WoZStudy/COACH/defines.h
+++ WoZStudy/WoZStudy/COACH/defines.h
@@ -2,14 +2,15 @@
 
 #define NAO_PROMPT
 
-#define USER_NAME "MJ"
+#define USER_NAME "Ann"
 
 #define TIME_WAIT_PROMPT    1
 #define TIME_WAIT_TASK_FIN	30
 
 
 //#define  robotIP "127.0.0.1"
-#define  robotIP "192.168.1.3"
+//#define  robotIP "192.168.1.3"
+#define  robotIP "192.168.1.121"
 //#define  robotIP "192.168.0.1"
 #define robotPort 9559
 
